7bce75c4e06459a873a2a065aeba1ebb4778d0fd
westsider <whansen1@mac.com>
2024-01-03 08:51:06 -0600

    Initial Commit

---
+++ ButtonViewTest/ButtonViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonViewTestApp.swift
+//  ButtonViewTest
+//
+//  Created by Warren Hansen on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
